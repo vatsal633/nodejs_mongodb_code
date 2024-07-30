09669bf39a65318f46314b96db6ae444d73e8c16
vatsal2004 <153715547+vatsal633@users.noreply.github.com>
2024-07-30 11:01:31 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import express from "express";
-import {Todo} from "../models/todo";
+import Todo from "./models/todo.js";
 const app = express();
 const port = 8000;
 
@@ -8,11 +8,20 @@ let connection = await mongoose.connect("mongodb://localhost:27017/");
 
 
 app.get('/',(req,res)=>{
-    const todo = new Todo({title:"hey first to do",desc:"description of the todo",isdone:true})
-    todo.save()
-    res.send("hello world");
+    try{
+        const todo = new Todo({title:"hey first to do",desc:"description of the todo",isdone:true,days:7})
+        todo.save()
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.get('/f',async(req,res)=>{
+    let todo = await Todo.findOne({})
+    console.log(todo)
+    res.json({title : todo.title,desc : todo.desc})
 })
 
 app.listen(port,()=>{
     console.log(`this server runs port ${port}`)
-})  
+})
